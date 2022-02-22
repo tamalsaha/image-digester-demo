4fb8d69f22e0a560fb211e89de093779ef4ee081
Tamal Saha <tamal@appscode.com>
2022-02-21 19:21:35 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/tamalsaha/image-digester-demo/pkg/version"
+)
 
+func main() {
+	image := "kubedb/operator:v0.25.0-dbg_linux_arm64"
+	digest, err := crane.Digest(image,
+		crane.WithUserAgent(fmt.Sprintf("%s/%s", "digester", version.Version)))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(digest)
 }
